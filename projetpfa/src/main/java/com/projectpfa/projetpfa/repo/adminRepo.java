417e7a11bd1aff6298d8d50b75a13ddefcb2df19
Bouazza Chaymae <108526847+Shiame@users.noreply.github.com>
2024-06-03 14:52:35 +0100

    Delete projetpfa directory

--- projetpfa/src/main/java/com/projectpfa/projetpfa/repo/adminRepo.java
+++
@@ -1,7 +0,0 @@
-package com.projectpfa.projetpfa.repo;
-
-import com.projectpfa.projetpfa.model.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface adminRepo extends JpaRepository<Admin, Integer> {
-}
