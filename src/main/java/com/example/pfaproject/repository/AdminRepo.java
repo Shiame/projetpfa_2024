3c3f70ee5ce9597e3ac86465bfe379bbaabd46bb
Bouazza Chaymae <108526847+Shiame@users.noreply.github.com>
2024-06-12 22:50:44 +0100

    Delete src/main/java/com/example/pfaproject directory

--- src/main/java/com/example/pfaproject/repository/AdminRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.autocar.Repository;
-
-import com.example.autocar.models.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepo extends JpaRepository<Admin, Integer> {
-}
